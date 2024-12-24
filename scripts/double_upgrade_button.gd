167637103ef1535c5460fa335c8afc40ad7ed446
Pierre Cuquel <p.cuquel@free.fr>
2024-12-24 13:37:23 +0100

    Added french translation

--- scripts/double_upgrade_button.gd
+++ scripts/double_upgrade_button.gd
@@ -2,8 +2,8 @@ class_name DoubleUpgradeButton extends Button
 
 func set_upgrades(upgrade: Upgrade, repeatable: RepeatableUpgrade):
 	%IconBase.texture = upgrade.icon
-	%TitleBase.text = "[center][font_size=16]" + upgrade.name
-	%DescriptionBase.text = "[font_size=12]" + upgrade.description
+	%TitleBase.text = "[center][font_size=16]" + tr(str(upgrade.id) + "_NAME")
+	%DescriptionBase.text = "[font_size=12]" + tr(str(upgrade.id) + "_DESC")
 	%IconRepeatable.texture = repeatable.icon
-	%TitleRepeatable.text = "[center][font_size=16]" + repeatable.name
+	%TitleRepeatable.text = "[center][font_size=16]" + tr(str(repeatable.id) + "_NAME")
 	pressed.connect($/root/Main._on_upgrade_button_pressed.bind(upgrade.id, repeatable.id))
